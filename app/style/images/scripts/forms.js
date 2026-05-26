02899fe92ccfce6f15adff1b07ad67ca1519d63c
FELLIPE DE SOUZA BERNARDES <05622358094@senacrs.edu.br>
2026-05-26 17:05:21 -0300

    Forms Updated

---
+++ app/style/images/scripts/forms.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('post-form');
+
+form.addEventListener('submit', function(event) {
+
+        event.preventDefault();
+
+        const autorInput = document.getElementById('autor')
+        const tituloInput = document.getElementById('titulo')
+        const historiaInput = document.getElementById('historia')
+        const historiasContainer = document.getElementById('historias')
+
+        const autor = autorInput.value;
+        const titulo = tituloInput.value;
+        const historia = historiaInput.value;
+
+        if (!autor || !historia) { //verificação de campos vazios
+               alert ("Os campos Nome e História são obrigatórios!")
+               return
+        }
+
+        const article = document.createElement('article');
+        article.className = "artigo";
+
+        article.innerHTML =
+                `<h3>${titulo}</h3>
+         <p><strong> Autor: ${autor} </strong></p>
+         <p>${historia.replace('/\n/g', '<br />')}</p>
+         <hr />`;
+
+         document.getElementById('historias').appendChild(article)
+
+         event.target.reset()
+})
