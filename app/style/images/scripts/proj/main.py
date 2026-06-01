b785375392e5c6424ffe37963e44bb2b65f865da
FELLIPE DE SOUZA BERNARDES <05622358094@senacrs.edu.br>
2026-06-01 17:25:58 -0300

    proj dir

---
+++ app/style/images/scripts/proj/main.py
@@ -0,0 +1,9 @@
+def triangulo_base_altura(b: float, a: float):
+...     print(f"A área do triângulo é {b * a / 2}")
+
+def temp_conversao(c: float):
+...     print(f"A temperatura em fahrenreit é {c / 5 * 9 + 32}")
+
+def reais_conversao(r: float):
+...          print(f"O valor em dolares é {r / 5.03}")
+
