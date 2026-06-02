a85d9ff5cf3caf35c4cc1ec6e77794ed41d63d9d
FELLIPE DE SOUZA BERNARDES <05622358094@senacrs.edu.br>
2026-06-02 15:07:45 -0300

    Main.py and proj dir

--- app/style/images/scripts/proj/main.py
+++ app/style/images/scripts/proj/main.py
@@ -1,9 +1,21 @@
 def triangulo_base_altura(b: float, a: float):
-...     print(f"A área do triângulo é {b * a / 2}")
+print(f"A área do triângulo é {b * a / 2}")
 
 def temp_conversao(c: float):
-...     print(f"A temperatura em fahrenreit é {c / 5 * 9 + 32}")
+print(f"A temperatura em fahrenreit é {c / 5 * 9 + 32}")
 
 def reais_conversao(r: float):
-...          print(f"O valor em dolares é {r / 5.03}")
+    print(f"O valor em dolares é {r / 5.03}")
+
+def calc_salario_desconto(S: float):
+Sd = S - S*0.15
+return Sd
+
+def Club_Felps(nome: str, idade: int):
+    if nome == "felps" and idade >= 16:
+        print("Voce é membro do clube!")
+            else:
+                print("Vaza daqui meo!")
+
+
 
