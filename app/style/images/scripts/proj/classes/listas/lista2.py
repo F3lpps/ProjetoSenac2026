1977a46b2f97accaec7d6b8ab85e1118a8a1ff0f
FELLIPE DE SOUZA BERNARDES <05622358094@senacrs.edu.br>
2026-06-08 17:29:59 -0300

    Python ex

--- app/style/images/scripts/proj/classes/listas/lista2.py
+++ app/style/images/scripts/proj/classes/listas/lista2.py
@@ -1,20 +1,20 @@
 def triangulo_base_altura(b: float, a: float):
-print(f"A área do triângulo é {b * a / 2}")
+    print(f"A área do triângulo é {b * a / 2}")
 
 def temp_conversao(c: float):
-print(f"A temperatura em fahrenreit é {c / 5 * 9 + 32}")
+    print(f"A temperatura em fahrenreit é {c / 5 * 9 + 32}")
 
 def reais_conversao(r: float):
     print(f"O valor em dolares é {r / 5.03}")
 
 def calc_salario_desconto(S: float):
-Sd = S - S*0.15
-return Sd
+    Sd = S - S*0.15
+    return Sd
 
 def Club_Felps(nome: str, idade: int):
     if nome == "felps" and idade >= 16:
         print("Voce é membro do clube!")
-            else:
+    else:
                 print("Vaza daqui meo!")
 
 def class_population(idade: int) -> str:
@@ -28,9 +28,9 @@ def class_population(idade: int) -> str:
 
 def eh_par(numero:int):
     if numero % 2:
-return true 
+        return True
     else:
-        return false
+        return False
 
 def calcular_bonus(S:float, a_empresa:int):
     if a_empresa > 5:
@@ -38,7 +38,7 @@ def calcular_bonus(S:float, a_empresa:int):
     else:
         return S * 0.05    
 
-def encontrar_maior(a: float or int, b: float or int, c: float or int):
+def encontrar_maior(a: float, b: float, c: float):
     if a > b and a > c:
         return a
     if c > b and c > a:
@@ -47,35 +47,31 @@ def encontrar_maior(a: float or int, b: float or int, c: float or int):
         return b   
 
 def tipo_triangulo(lado1, lado2, lado3):
-...         if lado1 + lado2 > lado3
-                or lado2 + lado3 > lado1
-                    or lado1 + lado3 > lado2:
-...         return "Equilátero"
-...         if lado1 + lado2 < lado3:
-...                 return "Isto não é um triângulo"
-...         if lado1 + lado2 == lado3:
-...                 return "Isósceles"
-...         if lado1 + lado2 + lado3 == lado1 != lado2 != lado3:
-...                 return "Escaleno"
+ if lado1 + lado2 > lado3:
+            if lado2 + lado3 > lado1:
+                                        lado1 + lado3 > lado2
+            return "Equilátero"
+            if lado1 + lado2 < lado3:
+                return "Isto não é um triângulo"
+            if lado1 + lado2 == lado3:
+                return "Isósceles"
+            if lado1 + lado2 + lado3 == lado1 != lado2 != lado3:
+                return "Escaleno"
 
 def aprovar_saque(saldo:float, valor_saque: float):
     if valor_saque <= saldo and valor_saque % 10 == 0:
-        return true
+        return True
     else:
-        return false 
-
-if __name__ == '__main__'
-    fruta = primeira_fruta(frutas)
-        print(fruta)
+        return False 
 
     frutas = ['Laranja','banana','berga','maça',]
 
-def primeira_fruta(frutas: list)
+def primeira_fruta(frutas: list):
         return primeira_fruta[0]
 
 animais = ['Gato','Raposa','cachorro','Koala']
-    def ultimo_animal(animais: list)
-        return ultimo_animal[-1]    
+def ultimo_animal(animais: list):
+        return ultimo_animal[-1]   
 
          
 
